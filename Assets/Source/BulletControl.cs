df7c0827110b78ce035c8ed5f0d73578bbf9823e
stories2 <stories2@naver.com>
2017-03-05 08:14:26 +0900

    BulletModule, PlayerModule : shoot bullet
    플레이어로 부터 자동적으로 일정 시간마다 총알을 발사할 수 있으며 각 총알은 화면을 벗어날 시 자동적으로 사라진다

---
+++ Assets/Source/BulletControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour {
+
+	const float END_OF_TOP_SCREEN = 1.0f, END_OF_BOTTOM_SCREEN = -1.0f;
+
+	Vector3 bulletPosition;
+	GameObject eachBulletObject;
+
+	// Use this for initialization
+	void Start () {
+		bulletPosition = new Vector3 ();
+		eachBulletObject = gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		bulletPosition = eachBulletObject.transform.position;
+		if (END_OF_TOP_SCREEN < bulletPosition.y || bulletPosition.y < END_OF_BOTTOM_SCREEN) {
+			Destroy (eachBulletObject);
+		}
+		eachBulletObject.transform.Translate(Vector3.forward * Time.deltaTime);
+		eachBulletObject.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+	}
+}
---
+++ Assets/Source/PlayerShootManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootManager : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	GameObject playerObject;
+	float bulletShootGabTime, bulletShootLastTime;
+
+	// Use this for initialization
+	void Start () {
+		playerObject = gameObject;
+		bulletShootGabTime = 0.125f;
+		bulletShootLastTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bulletPrefab != null && IsTimeToShootAnotherBullet()) {
+			Instantiate (bulletPrefab, playerObject.transform.position, Quaternion.identity);
+		}
+	}
+
+	bool IsTimeToShootAnotherBullet() {
+		if (bulletShootLastTime > bulletShootGabTime) {
+			bulletShootLastTime = 0;
+			return true;
+		}
+		else {
+			bulletShootLastTime = bulletShootLastTime + Time.deltaTime;
+		}
+		return false;
+	}
+}
