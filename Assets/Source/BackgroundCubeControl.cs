83cf781b134cb0b36c9a08033c20e5f185be12ad
stories2 <stories2@naver.com>
2017-03-25 02:51:11 +0900

    BackgroundModule : Create Background Cubes
    배경에 큐브가 떨어지게 함으로써 뭔가 있어보이게 함

---
+++ Assets/Source/BackgroundCubeControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCubeControl : DefineManager {
+	public float speed = 5f, fallingSpeed = 0.2f;
+	Vector3 rotateDirection;
+	GameObject backgroundCubeObject;
+
+	// Use this for initialization
+	void Start () {
+		backgroundCubeObject = gameObject;
+		fallingSpeed = Random.Range (0.2f, 0.5f) + 0.2f;
+		rotateDirection = new Vector3 (Random.Range (0.5f, 1.5f) + 0.5f, Random.Range (0.5f, 1.5f) + 0.5f, Random.Range (0.5f, 1.5f) + 0.5f);
+	}
+
+	void Update ()
+	{
+		backgroundCubeObject.transform.Rotate(rotateDirection, speed * Time.deltaTime);
+		//backgroundCubeObject.transform.Translate(Vector3.forward * Time.deltaTime);
+		backgroundCubeObject.transform.Translate(Vector3.down * Time.deltaTime * fallingSpeed, Space.World);
+
+		AutoDestroy (backgroundCubeObject.transform.position, backgroundCubeObject);
+	}
+
+	void AutoDestroy(Vector3 myPosition, GameObject myObject) {
+		if (myPosition.y < END_OF_BOTTOM_SCREEN_WIDTH_GAP) {
+			Destroy (myObject);
+		}
+	}
+}
---
+++ Assets/Source/BackgroundFallingCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFallingCube : DefineManager {
+
+	public GameObject theCube;
+
+	int i, createCubeLimit, createCubeNumThisTime;
+	float createCubeTimeLimit, createCubeTimer;
+	Vector3 createCubePosition;
+
+	// Use this for initialization
+	void Start () {
+		createCubeTimer = 0;
+		createCubeTimeLimit = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		createCubeTimer += Time.deltaTime;
+		if (createCubeTimeLimit < createCubeTimer) {
+			createCubeTimer = 0;
+			createCubeLimit = Random.Range (1, 4);
+			for (i = 0; i < createCubeLimit; i += 1) {
+				createCubePosition = new Vector3 (2.0f / (createCubeLimit + 1) * (i + 1) - 1.0f, END_OF_TOP_SCREEN_TOP_GAP, 5);
+				Instantiate (theCube, createCubePosition, Quaternion.identity);
+			}
+		}
+	}
+}
